f6830edeea216a1b75fcc94385d58636c47f8bcf
wangzhifang <2392756129@qq.com>
2020-12-30 11:14:33 +0800

    测试Git，将idea代码上传

---
+++ src/com/GitTest.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * @author 王智芳
+ * @data 2020/12/30 8:56
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("测试idea关联Git，并上传");
+    }
+}
